6721a0762c5182d6c1ad56d080140f9af4b3e991
wanqiang <wanqiang@longshine.com>
2024-11-01 17:57:33 +0800

    version 0.0.1

---
+++ ORFramework/Classes/ORTestApi.m
@@ -0,0 +1,17 @@
+//
+//  ORTestApi.m
+//  ORFramework
+//
+//  Created by longshine on 2024/11/1.
+//  Copyright © 2024 wanqiang. All rights reserved.
+//
+
+#import "ORTestApi.h"
+
+@implementation ORTestApi
+
++ (void)testApi{
+    NSLog(@"123");
+}
+
+@end
